41ac4ef700b6aa7b13b93df8f16fc6e903fbbe03
Alden Weaver <alden@Ruthannes-MacBook-2.local>
2018-11-24 16:44:06 -0800

    Added async to run the app

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-
+/* LIBRARY IMPORTS */
+// const aysnc = require('aysnc');
+const async = require('async');
 const mysql = require('mysql2');
+const chance = require('chance').Chance();
+
+/* OBJECT IMPORTS */
+const Restaurant = require('./v1/src/objects/restaurant');
+const Check = require('./v1/src/objects/check');
+const Payment = require('./v1/src/objects/payment');
+
 
 const opts = {
     host: "localhost",
@@ -12,16 +21,85 @@ const opts = {
 
 let pool = mysql.createPool(opts);
 
-let restaurant = new Restaurant(pool);
 
-restaurant.generateMock();
-restaurant.insert(function(err, _restaurant) {
+async.parallel([
+    function(callback) {
+        async.forever(
+            function(next) {
+                /* TEST RESTAURANT CREATION & INSERTION */
+                let restaurant = new Restaurant(pool);
+
+                restaurant.generateMock();
+                restaurant.insert(function(err, _restaurant) {
+                    if(err) {
+                        return next(err);
+                    } else {
+                        console.log("Successfully created restaurant: ", _restaurant);
+                        return next();
+                    }
+                });
+            },
+            function(err) {
+                return callback(err);
+            }
+        );
+    },
+    function(callback) {
+        async.forever(
+            function(next) {
+                async.waterfall([
+                    function(callback) {
+                        Restaurant.getRandId(pool, function (err, _restaurantId) {
+                            if(err) {
+                                return callback(err);
+                            } else {
+                                return callback(null, _restaurantId);                            
+                            }
+                        });
+                    },
+                    // Result from above is passed as restaurantId
+                    function(restaurantId, callback) {
+                        let check = new Check(pool, restaurantId);
+
+                        check.generateMock();
+                        check.insert(function(err, _check) {
+                            if(err) {
+                                return callback(err);                            
+                            } else {
+                                console.log("Successfully created check: ", _check);
+                                return callback(null, _check);  
+                            }
+                        });
+                    },
+                    function(check, callback) {
+                        let payment = new Payment(pool, check.id, check.total, chance.cc());
+
+                        payment.insert(function(err, _payment) {
+                            if(err) {
+                                return callback(err);                            
+                            } else {
+                                console.log("Successfully created payment: ", _payment);
+                                return callback(null, _payment);  
+                            }
+                        });
+                    }
+                ], function (err, payment) {
+                        if(err) {
+                            return next(err);
+                        } else {
+                            return next();
+                        }
+                    });
+            },
+            function(err) {
+                return callback(err);
+            }
+        );
+    }
+], function(err, results) {
     if(err) {
         console.log("Exited with error: ", JSON.stringify(err));
     } else {
-        console.log("Successfully created & inserted new restaurant object: ", 
-                        _restaurant);
+        console.log("Results: ", results);
     }
 });
-
-// this.restaurant_id = chance.integer({ min: 1, max: Restaurant.getMaxID(this.connectionPool, this.restaurant_id) });
